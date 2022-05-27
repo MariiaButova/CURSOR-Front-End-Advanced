55a4376e4ca0cac88c090064e1362c6bfb867407
Mariia Butova <kasha.butova@gmail.com>
2022-05-28 00:01:21 +0300

    Changed code in Homework_1.2 folder

--- Homework_1.2/main.js
+++ Homework_1.2/main.js
@@ -15,10 +15,10 @@ console.log(`Загальна вартість всіх товарів: ${TOTAL_
 const TOTAL_PRICE_WITHOUT_PENNIES = Math.floor(ORANGE_PRICE) + Math.floor(BANANA_PRICE) + Math.floor(EGG_PRISE);
 console.log(`Загальна вартість товарів без копійок: ${TOTAL_PRICE_WITHOUT_PENNIES}`);
 
-const ROUNDED_TOTAL_PRICE = Math.round( TOTAL_PRICE_WITHOUT_PENNIES / 100)*100;
+const ROUNDED_TOTAL_PRICE = Math.round( TOTAL_PRICE_WITHOUT_PENNIES / 100) * 100;
 console.log(`Сума товарів округлена до сотень: ${ROUNDED_TOTAL_PRICE}`);
 
-const IS_EVEN_NUMBER = Math.floor(TOTAL_PRICE) %2 === 0;
+const IS_EVEN_NUMBER = Math.floor(TOTAL_PRICE) % 2 === 0;
 console.log(`Чи парне число: ${IS_EVEN_NUMBER}`);
 
 const REMAINDER = 500 - TOTAL_PRICE;
@@ -27,10 +27,10 @@ console.log(`Решта: ${REMAINDER}`);
 const AVERAGE_PRICE = (TOTAL_PRICE / 3).toFixed(2)
 console.log(`Середнє значення цін: ${AVERAGE_PRICE}`);
 
-const DISCOUNT = Math.round(Math.random()* 100);
+const DISCOUNT = Math.round(Math.random() * 100);
 console.log(`Випадкова знижка: ${DISCOUNT}%`);
 
-const PRICE_WITH_DISCOUNT = (TOTAL_PRICE / 100 *(100 - DISCOUNT)).toFixed(2);
+const PRICE_WITH_DISCOUNT = (TOTAL_PRICE / 100 * (100 - DISCOUNT)).toFixed(2);
 console.log(`Ціна зі знижкою: ${PRICE_WITH_DISCOUNT}`);
 
 const NET_PROFIT = ((TOTAL_PRICE / 2) - (TOTAL_PRICE / 100 * DISCOUNT)).toFixed(2);
