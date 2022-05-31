155a9344d74d865a13a0fd53dee7bdb9fb027b2e
Mariia Butova <kasha.butova@gmail.com>
2022-05-31 16:45:15 +0300

    Changed code in main,js file

--- Homework_2/main.js
+++ Homework_2/main.js
@@ -1,11 +1,11 @@
 let n;
 do {
-  n = +prompt(`Введіть число N`, 2)
+  n = +prompt(`Введіть число N`)
 } while (n % 1 !== 0);
 
 let m;
 do {
-  m = +prompt(`Введіть число M`, 2)
+  m = +prompt(`Введіть число M`)
 } while (m % 1 !== 0 || m <= n);
 
 let passEvenNumber = confirm(`Чи потрібно пропускати парні числа?`);
